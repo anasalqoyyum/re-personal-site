a9d3d1de1e936c7ab568c318291d81c28a801b70
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2022-09-01 05:46:20 +0700

    feat: prepare syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,15 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), image(), mdx()]
+  site: 'https://www.anasalqoyyum.com',
+  integrations: [tailwind(), preact(), image(), mdx()],
+  markdown: {
+		syntaxHighlight: false,
+		rehypePlugins: [
+			'rehype-slug',
+			'rehype-autolink-headings',
+			'rehype-code-titles',
+			'rehype-prism',
+		],
+	},
 });
